ac4b064a079217572ea2c007a082ec0aeb5d1da7
Agustin Mites <mites96@hotmail.com>
2026-02-07 11:11:25 -0500

    feat(frontend): integrate backend services and live data consumption

--- app/backend-microservice/report/src/main/java/com/microservice/report/ReportApplication.java
+++ app/backend-microservice/report/src/main/java/com/microservice/report/ReportApplication.java
@@ -2,6 +2,9 @@ package com.microservice.report;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ReportApplication {
@@ -10,4 +13,16 @@ public class ReportApplication {
 		SpringApplication.run(ReportApplication.class, args);
 	}
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
